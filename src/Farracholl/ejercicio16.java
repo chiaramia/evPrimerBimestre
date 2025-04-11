0e1a13eb8cd266b30858b3161add8bdca3d91b66
Chiaramia <49006555@fatimarem.edu.ar>
2025-04-11 11:28:49 -0300

    Subo evaluacion

---
+++ src/Farracholl/ejercicio16.java
@@ -0,0 +1,48 @@
+package Farracholl;
+
+import java.util.Scanner;
+
+public class ejercicio16 {
+public static void main(String[] args) {
+	Scanner ingreso = new Scanner(System.in);
+	
+	int opciones;
+	
+	System.out.println("Elegir entre las siguentes opciones :");
+	System.out.println("1");
+	System.out.println("2");
+	System.out.println("3");
+	System.out.println("4");
+	System.out.println("5");
+	
+	opciones = ingreso.nextInt();
+	
+	switch(opciones) {
+	 
+	case 1: System.out.println("Cafe Cortado");
+	break;
+	
+	 
+	case 2: System.out.println("Cafe Latte");
+	break;
+	
+	
+	case 3: System.out.println("Cafe Solo");
+	break;
+	
+	
+	case 4: System.out.println("Cafe Lagrima");
+	break;
+	
+
+	case 5: System.out.println("Saliste del programa");
+	break;
+	
+	default: System.err.println("Elegiste cualquier cosa");
+	break;
+	}
+}
+
+
+}
+
---
+++ src/Farracholl/ejercicio19.java
@@ -0,0 +1,40 @@
+package Farracholl;
+
+import java.util.Scanner;
+
+public class ejercicio19 {
+	public static void main(String[] args) throws InterruptedException {
+		Scanner ingreso = new Scanner (System.in);
+		
+		int contrasena;
+		int intentos = 0;
+		
+		
+
+		do {
+
+			System.out.println("Ingresa tu contrasena: ");
+			contrasena = ingreso.nextInt();
+
+			if (contrasena != 2008) {
+				Thread.sleep(100);
+
+				intentos++; 
+
+				if (intentos < 3) {
+					System.out.println("Volve a intentarlo. ");
+				}
+
+			}
+
+		} while (contrasena != 2008 && intentos < 3);
+
+		if (contrasena == 2008) {
+			System.out.println("Contrasena correcta.");
+		}
+		if (contrasena != 2008) {
+			System.err.println("Bloqueaste el usuario, salame. ");
+		}
+	}
+
+}
